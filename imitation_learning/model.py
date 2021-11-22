124fc4effe5f798ffa8e1e41840ce2167b669e5b
ShawX825 <shawliuxiao@gmail.com>
2021-11-22 20:12:24 +0800

    modified code

--- imitation_learning/model.py
+++ imitation_learning/model.py
@@ -215,4 +215,146 @@ class Autoencoder(nn.Module):
         x = self.linear_head(x)
         return x
 
+def init_weights(net, init_type='normal', gain=0.02):
+    def init_func(m):
+        classname = m.__class__.__name__
+        if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
+            if init_type == 'normal':
+                init.normal_(m.weight.data, 0.0, gain)
+            elif init_type == 'xavier':
+                init.xavier_normal_(m.weight.data, gain=gain)
+            elif init_type == 'kaiming':
+                init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+            elif init_type == 'orthogonal':
+                init.orthogonal_(m.weight.data, gain=gain)
+            else:
+                raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
+            if hasattr(m, 'bias') and m.bias is not None:
+                init.constant_(m.bias.data, 0.0)
+        elif classname.find('BatchNorm2d') != -1:
+            init.normal_(m.weight.data, 1.0, gain)
+            init.constant_(m.bias.data, 0.0)
+
+    print('initialize network with %s' % init_type)
+    net.apply(init_func)
+
+class conv_block(nn.Module):
+    def __init__(self,ch_in,ch_out):
+        super(conv_block,self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(ch_in, ch_out, kernel_size=3,stride=1,padding=1,bias=True),
+            nn.BatchNorm2d(ch_out),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(ch_out, ch_out, kernel_size=3,stride=1,padding=1,bias=True),
+            nn.BatchNorm2d(ch_out),
+            nn.ReLU(inplace=True)
+        )
+
+
+    def forward(self,x):
+        x = self.conv(x)
+        return x
+
+class up_conv(nn.Module):
+    def __init__(self,ch_in,ch_out):
+        super(up_conv,self).__init__()
+        self.up = nn.Sequential(
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(ch_in,ch_out,kernel_size=3,stride=1,padding=1,bias=True),
+		    nn.BatchNorm2d(ch_out),
+			nn.ReLU(inplace=True)
+        )
+
+    def forward(self,x):
+        x = self.up(x)
+        return x
+
+class U_Net(nn.Module):
+    def __init__(self,img_ch=CHANNEL, num_action=5, dim=64, map_size=12):
+        super(U_Net,self).__init__()
+        
+        self.map_size = map_size
+        
+        self.Maxpool = nn.MaxPool2d(kernel_size=2,stride=2)
+
+        self.Conv1 = conv_block(ch_in=img_ch,ch_out=dim)
+        self.Conv2 = conv_block(ch_in=dim,ch_out=dim*2)
+        self.Conv3 = conv_block(ch_in=dim*2,ch_out=dim*4)
+        
+        if map_size >=24:
+            self.Conv4 = conv_block(ch_in=dim*4,ch_out=dim*4)
+        # self.Conv5 = conv_block(ch_in=512,ch_out=1024)
+
+        # self.Up5 = up_conv(ch_in=1024,ch_out=512)
+        # self.Up_conv5 = conv_block(ch_in=1024, ch_out=512)
+
+            self.Up4 = up_conv(ch_in=dim*4,ch_out=dim*4)
+            self.Up_conv4 = conv_block(ch_in=dim*8, ch_out=dim*4)
+        
+        self.Up3 = up_conv(ch_in=dim*4,ch_out=dim*2)
+        self.Up_conv3 = conv_block(ch_in=dim*4, ch_out=dim*2)
+        
+        self.Up2 = up_conv(ch_in=dim*2,ch_out=dim)
+        self.Up_conv2 = conv_block(ch_in=dim*2, ch_out=dim)
+
+        # self.Conv_1x1 = nn.Conv2d(64,output_ch,kernel_size=1,stride=1,padding=0)
+        
+        self.linear_head = nn.Sequential(
+                                    nn.Dropout(0.5),
+                                    nn.Linear(dim, dim),
+                                    nn.BatchNorm1d(dim),
+                                    nn.ReLU(),
+                                    nn.Dropout(p=0.5),
+                                    nn.Linear(dim, num_action)
+                                    )
+    def encode(self, x):
+        # encoding path
+        x1 = self.Conv1(x)
+
+        x2 = self.Maxpool(x1)
+        x2 = self.Conv2(x2)
+        
+        x3 = self.Maxpool(x2)
+        x3 = self.Conv3(x3)
+
+        
+
+        # x5 = self.Maxpool(x4)
+        # x5 = self.Conv5(x5)
+
+        # decoding + concat path
+        # d5 = self.Up5(x5)
+        # d5 = torch.cat((x4,d5),dim=1)
+        
+        # d5 = self.Up_conv5(d5)
+        
+        if self.map_size >= 24:
+            x4 = self.Maxpool(x3)
+            x4 = self.Conv4(x4)
+            d4 = self.Up4(x4)
+            d4 = torch.cat((x3,d4),dim=1)
+            x3 = self.Up_conv4(d4)
+
+        d3 = self.Up3(x3)
+        d3 = torch.cat((x2,d3),dim=1)
+        d3 = self.Up_conv3(d3)
+
+        d2 = self.Up2(d3)
+        d2 = torch.cat((x1,d2),dim=1)
+        d2 = self.Up_conv2(d2)
+        
+        d2 = (d2 * x[:,:1]).view(d2.size(0), d2.size(1), -1).sum(-1) 
+        return d2
+
+    def forward(self,x):
+        
+        d2 = self.encode(x)
+        d2 = self.linear_head(d2)
+
+        # d1 = self.Conv_1x1(d2)
+
+        return d2
+
+
+
     
